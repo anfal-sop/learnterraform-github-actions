29f4938396ae5bd3e9b630a3cfcf9a3611f5f7e1
anfal-sop <157883079+anfal-sop@users.noreply.github.com>
2024-02-19 18:15:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,9 +9,8 @@ terraform {
 
 resource "aws_s3_bucket" "test_bucket" {
   bucket = "my-unique-bucket-name"
-   AWS_ACCESS_KEY_ID: ${{ secrets.AWS_ACCESS_KEY_ID }}
-AWS_SECRET_ACCESS_KEY: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-  # Other bucket configurations...
+
+  # Other bucket configurations
 
   # Define an access control policy
   policy = jsonencode({
