8e63b062cff46e91bc708b6903b3ba582e1bf790
anfal-sop <157883079+anfal-sop@users.noreply.github.com>
2024-02-19 18:06:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,22 +7,7 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-1"
-
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name"
-  
-  # Define an access control policy
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Principal = "*",
-        Action   = "*",
-        Resource = "arn:aws:s3:::${aws_s3_bucket.my_bucket.bucket}/*",
-      },
-    ],
-  })
+  acl    = "private"
 }
