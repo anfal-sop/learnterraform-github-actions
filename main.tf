f625fc71a5467ef6b1cba94613e3bdcc46f7149c
anfal-sop <157883079+anfal-sop@users.noreply.github.com>
2024-02-20 17:21:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,39 +28,49 @@ provider "aws" {
 #  depends_on = [module.BackupIAM]
 #}
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "my_tfstate_bucket"
-  acl    = "private"
+# Step 1 - Resource block for EC2 instance
+resource "aws_instance" "ec2_example" {
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  ami                    = "ami-0767046d1677be5a0"
+  instance_type          = "t2.micro"
+  key_name               = "aws_key"
+  vpc_security_group_ids = [aws_security_group.main.id]
+}
+
+# Step 2 - Resource block for Security Group
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0",]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
     }
-  }
+  ]
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0",]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
 }
 
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
-  
-  # Deny non-https request policy
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "ForceSSLOnlyAccess"
-        Effect    = "Deny"
-        Principal = "*"
-        Action    = "s3:*"
-        Resource  = "arn:aws:s3:::my_tfstate_bucket/*"
-        Condition = {
-          Bool = {
-            "aws:SecureTransport" = "false"
-          }
-        }
-      },
-    ]
-  })
-}
+# Step 3 - Set up the SSH key pair
+# To generate SSH key refer to - https://jhooq.com/terraform-generate-ssh-key
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "<PLACE-YOUR-PUBLIC-KEY>"
+
 
