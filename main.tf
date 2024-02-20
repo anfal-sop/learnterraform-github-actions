d5f15eb01a26c9bcff7b7da5ebcde28d8d8349ae
anfal-sop <157883079+anfal-sop@users.noreply.github.com>
2024-02-20 17:05:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,3 +28,39 @@ provider "aws" {
 #  depends_on = [module.BackupIAM]
 #}
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my_tfstate_bucket"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  
+  # Deny non-https request policy
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "ForceSSLOnlyAccess"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource  = "arn:aws:s3:::my_tfstate_bucket/*"
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
+
